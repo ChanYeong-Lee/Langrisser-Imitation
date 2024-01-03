5d09398339acc16ded9ae50f18421561f6de75ef
ChanYeong-Lee <zizonxm@naver.com>
2024-01-04 04:48:18 +0900

    Initial commit

---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float zoomSpeed = 3;
+    public float moveSpeed = 3;
+
+    [Header("Limit")]
+    public Vector2 minVector;
+    public Vector2 maxVector;
+
+    public Transform player;
+    void Update()
+    {
+        Zoom();
+        MouseMove();
+        KeyboardMove();
+        CheckNode();
+    }
+
+    private void Zoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 Dest = transform.position + transform.forward * scroll * zoomSpeed;
+        Dest.z = Mathf.Clamp(Dest.z, minVector.y, maxVector.y);
+        if (Dest.y < 6 && 2 < Dest.y)
+        {
+            transform.position = Dest;
+        }
+    }
+
+    private bool isDragging = false;
+    private Vector3 offset = Vector3.zero;
+    private Vector3 mouseOrigin = Vector3.zero;
+    private Vector3 target = Vector3.zero;
+
+    private void MouseMove()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+            mouseOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            offset = GetTarget();
+        }
+        if (isDragging)
+        {
+            target = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            Vector3 delta = (mouseOrigin - target);
+            delta.z = delta.y * 9/16;
+            delta.y = 0;
+            Follow(delta * 5 * (transform.position.y - 1) + offset);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+    }
+
+    private void KeyboardMove()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 dir = new Vector2(x, y);
+        dir = dir.normalized * Time.deltaTime * moveSpeed;
+        Move(dir);
+    }
+
+    private void Move(Vector2 dir)
+    {
+        float nextX = transform.position.x + dir.x;
+        float nextZ = transform.position.z + dir.y;
+        if (nextX < minVector.x || nextZ < minVector.y)
+            return;
+        if (maxVector.x < nextX || maxVector.y < nextZ)
+            return;
+        transform.Translate(new Vector3(dir.x, 0, dir.y), Space.World);
+    }
+
+    private void CheckNode()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit = ClickHitInfo();
+            if (hit.transform.GetComponent<Node>())
+            {
+                print("You Click Node");
+            }
+        }
+    }
+
+    private void Follow(Vector3 pos)
+    {
+        float x = pos.x;
+        x = Mathf.Clamp(x, minVector.x, maxVector.x);
+        float z = pos.z - transform.position.y / Mathf.Tan(Mathf.Deg2Rad * 50);
+        z = Mathf.Clamp(z, minVector.y, maxVector.y);   
+        transform.position = new Vector3(x, transform.position.y, z);
+    }
+
+    private Vector3 GetTarget()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+            return hit.point;
+        else
+            return Vector3.zero;
+    }
+
+    private RaycastHit ClickHitInfo()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hit);
+        return hit;
+    }
+}
---
+++ Assets/Scripts/SnowGenerator.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+    public float initX;
+    private Queue<GameObject> queue = new Queue<GameObject>();
+
+    private void Start()
+    {
+        PreGeneration();
+        StartCoroutine(GenerateCoroutine());
+    }
+
+    private void PreGeneration()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            GameObject snow = Instantiate(prefab);
+            snow.transform.parent = this.transform;
+            snow.SetActive(false);
+            queue.Enqueue(snow);
+        }
+    }
+    IEnumerator GenerateCoroutine()
+    {
+        while (true)
+        {
+            float rndY = Random.Range(-4f, 10f);
+            float rndScale = Random.Range(0.1f, 0.8f);
+            float rndTime = Random.Range(0.05f, 0.5f);
+            GameObject snow;
+            if (queue.Count > 0)
+            {
+                snow = queue.Dequeue();
+            }
+            else
+            {
+                snow = Instantiate(prefab);
+                snow.transform.parent = this.transform;
+            }
+            snow.transform.position = new Vector2(initX, rndY);
+            snow.transform.localScale = new Vector2(rndScale, rndScale);
+            snow.SetActive(true);
+            snow.GetComponent<Rigidbody2D>().AddForce(new Vector2(180, 0));
+            StartCoroutine(RemoveCoroutine(snow));
+            yield return new WaitForSeconds(rndTime);
+        
+        }
+    }
+
+    IEnumerator RemoveCoroutine(GameObject snow)
+    {
+        yield return new WaitForSeconds(20f);
+        snow.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        snow.SetActive(false);
+        queue.Enqueue(snow);
+    }
+}
